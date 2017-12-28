93f398faf18d76d87c5937555885f881a98f223b
JingjingHu <jingjing.hu@nodus.com>
2017-12-28 10:40:34 +0800

    Update RetrieveAll.cs

--- net40/GatewayAccount/RetrieveAll.cs
+++ net40/GatewayAccount/RetrieveAll.cs
@@ -27,7 +27,8 @@ namespace Samples.Net40
         {
             try
             {
-                var url = "https://sandbox.payfabric.com/rest/v1/api/setupid";
+                var url = "https://sandbox.payfabric.com/v1/rest/api/setupid";
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.Method = "GET";
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
@@ -46,7 +47,7 @@ namespace Samples.Net40
                 // Sample response
                 // ------------------------------------------------------
                 // Response text is an array of gateway account object with json format
-                // Go to https://github.com/PayFabric/APIs/wiki/API-Objects#gateway-account for more details about gateway account object.
+                // Go to https://github.com/PayFabric/APIs/blob/v1/Sections/Objects.md#gateway-account for more details about gateway account object.
                 // ------------------------------------------------------
 
             }
