1a33dd9a269d48cf1667612c62c61a9a5c0a9083
JingjingHu <jingjing.hu@nodus.com>
2017-12-28 10:41:43 +0800

    Update Retrieve.cs

--- net40/ShippingAddress/Retrieve.cs
+++ net40/ShippingAddress/Retrieve.cs
@@ -28,7 +28,8 @@ namespace Samples.Net40
         {
             try
             {
-                var url = "https://sandbox.payfabric.com/rest/v1/api/address" + "/" + addressId.ToString();
+                var url = "https://sandbox.payfabric.com/v1/rest/api/address" + "/" + addressId.ToString();
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.Method = "GET";
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
@@ -47,7 +48,7 @@ namespace Samples.Net40
                 // Sample response
                 // ------------------------------------------------------
                 // Response text is an address object with json format
-                // Go to https://github.com/PayFabric/APIs/wiki/API-Objects#address for more details about address object.
+                // Go to https://github.com/PayFabric/APIs/blob/v1/Sections/Objects.md#address for more details about address object.
                 // ------------------------------------------------------
 
             }
